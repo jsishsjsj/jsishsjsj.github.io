f9b99881ce58b4325a07f80e5a247b3dcf8519db
jsishsjsj <640997320@qq.com>
2025-03-02 15:51:46 +0800

    Site updated: 2025-03-02 15:51:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1.html.html","posts/2.html.html"];function toRandomPost(){
+var posts=["posts/1.html.html","posts/2.html.html","posts/2.html.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
