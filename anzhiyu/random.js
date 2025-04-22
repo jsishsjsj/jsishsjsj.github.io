86f6d2a27a516466e64a32580dd8b959231601e9
jsishsjsj <640997320@qq.com>
2025-04-22 20:56:03 +0800

    Site updated: 2025-04-22 20:56:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/ef6b.html.html","posts/2b3f.html.html","posts/806a.html.html","posts/d390.html.html"];function toRandomPost(){
+var posts=["posts/ef6b.html.html","posts/2b3f.html.html","posts/806a.html.html","posts/d390.html.html","posts/a097.html.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
