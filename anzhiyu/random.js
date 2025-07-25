b8dcb20718d45ddade27d4c24b7b991208599e09
jsishsjsj <640997320@qq.com>
2025-07-25 22:51:15 +0800

    Site updated: 2025-07-25 22:51:15

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["posts/ef6b.html.html","posts/2b3f.html.html","posts/806a.html.html","posts/d390.html.html"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
