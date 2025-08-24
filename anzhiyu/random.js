512e6572bf919257350eca9344d07d8fee486280
jsishsjsj <640997320@qq.com>
2025-08-24 22:41:33 +0800

    Site updated: 2025-08-24 22:41:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["posts/ef6b.html.html","posts/80fb.html.html","posts/2b3f.html.html","posts/66bb.html.html","posts/d363.html.html","posts/806a.html.html","posts/d390.html.html"];function toRandomPost(){
+var posts=["posts/e444.html.html","posts/80fb.html.html","posts/2b3f.html.html","posts/66bb.html.html","posts/d363.html.html","posts/806a.html.html","posts/d390.html.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
